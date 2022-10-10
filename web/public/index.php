789a43ecb3da371ea8a0ae1703ac4c9ec59d568b
charlie.perron_d_arc <charlie.perron_d_arc@etu.uca.fr>
2022-10-10 15:10:22 +0200

    Ajoute switch pour api + ajoute erreur api unknown action + supprime fonction

--- web/public/index.php
+++ web/public/index.php
@@ -1,42 +1,31 @@
 <?php
 require __DIR__ . '/../src/Controller/Database.php';
 
-function errorPage(int $code){
-	switch ($code) {
-		case '404':
-			$code="404 NOT_FOUND";
-			$message="Le paquet demandé n'existe pas";
-			break;
-		
-		default:
-			break;
-	}
-	print_r("<html><head><meta charset=\"utf-8\"><head/><title>$code</title>
-		<body><center><h1>$code</h1><hr/><h2>$message<h2/></center></body></html>");
-}
-
 $db = new Database();
 
 if(isset($_GET['api'])){
 	header('Content-type: application/json');
-	if($_GET['api'] == 'info'){
+
+	switch ($_GET['api']){
+
+	case 'info':
 		if(isset($_GET['package']))
 			$packageInfo=$db->getPackageInfo($_GET['package']);
 			if(empty($packageInfo))
 				echo json_encode(["error"=>"this packet does not exists"]);
 			else
 				echo json_encode($packageInfo);
-	}
+			break;
 
-	if($_GET['api'] == 'list'){
+	case 'list':
 		$packageList=$db->listPackages();
 		if(empty($packageList))
 			echo json_encode(["error"=>"there's no packages in the database"]);
 		else
 			echo json_encode($db->listPackages());
-	}
+		break;
 
-	if($_GET['api'] == 'version'){
+	case 'version':
 
 		if(empty($_GET['name']))
 			echo json_encode(["error"=>"must have a package name"]);
@@ -56,6 +45,11 @@ if(isset($_GET['api'])){
 					echo json_encode($version);
 			}
 		}
+		break;
+
+	default:
+		echo json_encode(["error"=>"unknown action"]);
+		break;
 	}
 }
 
