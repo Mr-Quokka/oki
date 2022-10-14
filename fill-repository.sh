174581b357cb4db54f5d5e2df6af6cba034192d8
clfreville2 <clement.freville2@etu.uca.fr>
2022-10-14 09:02:58 +0200

    *: Utilise des vrais paquets de test

---
+++ fill-repository.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eu
+
+if [ -d packages ]; then
+    cd packages && git pull --rebase --autostash && cd ..
+else
+    git clone https://codefirst.iut.uca.fr/git/oki/packages
+fi
+
+dest=$(dirname $(realpath "$0"))
+requests=''
+for package in packages/*; do
+    packageName=$(basename $package)
+    existingPackage="$dest"/web/public/packages/${packageName}_0.1.zip
+    rm "$existingPackage"
+    cd $package/src && zip -r "$existingPackage" ./*
+    cd -
+    requests="$requests
+    INSERT INTO package (short_name, long_name, langage_id) VALUES ('$packageName', 'A random package', (SELECT id_language FROM language WHERE designation = 'C'));
+    INSERT INTO version (package_id, identifier, published_date) VALUES ((SELECT id_package FROM package WHERE short_name='$packageName'), '0.1', CURRENT_TIMESTAMP);"
+done
+
+echo "$requests" | sqlite3 "$dest"/web/oki_packages.db
--- sql/create_sqlite_db.sh
+++ sql/create_sqlite_db.sh
@@ -8,13 +8,7 @@ sqlite3 ../web/oki_packages.db << EOF
 INSERT INTO language VALUES (1, 'C');
 INSERT INTO package VALUES (1, 'linked-list', 'My amazing linked list package', 1);
 INSERT INTO version VALUES (1,1,'1.0.0-ALPHA',CURRENT_TIMESTAMP);
-INSERT INTO package VALUES (2, 'mths', 'Min and max library', 1);
-INSERT INTO version VALUES (2, 2, '0.1', CURRENT_TIMESTAMP);
 INSERT INTO package VALUES (3, 'linked-list2', 'My second linked list package', 1);
-
 EOF
 
-if [ ! -f ../web/public/packages/linked-list_1.0.0-ALPHA.zip ] ; then
-    mkdir -p ../web/public/packages
-    touch ../web/public/packages/linked-list_1.0.0-ALPHA.zip
-fi
+../fill-repository.sh
