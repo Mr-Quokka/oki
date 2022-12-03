0987c4d0edc345a47e6e3ef88d3274ce032525e2
clfreville2 <clement.freville2@etu.uca.fr>
2022-12-03 12:10:01 +0100

    web: Refactor la vue d'inscription pour afficher les éventuelles erreurs

--- web/views/register.php
+++ web/views/register.php
@@ -1,11 +1,37 @@
 <?php $title = "Register"; ?>
+<?php if ($params['fail']) : ?>
+    <article class="message is-danger">
+        <div class="message-header">
+            <p>Registration failed</p>
+        </div>
+        <div class="message-body">
+            Login is already taken.
+        </div>
+    </article>
+<?php endif ?>
+<form action="<?= $_SERVER['REQUEST_URI'] ?>" method="post">
+    <div class="field">
+        <label class="label" for="login">Login</label>
+        <div class="control">
+            <input class="input" type="text" id="login" name="login" autocomplete="username">
+        </div>
+    </div>
+    <div class="field">
+        <label class="label" for="password">Password</label>
+        <div class="control">
+            <input class="input" type="password" id="password" name="password" autocomplete="new-password">
+        </div>
+    </div>
+    <div class="field">
+        <label class="label" for="password-confirmation">Password confirmation</label>
+        <div class="control">
+            <input class="input" type="password" id="password-confirmation" name="password-confirmation" autocomplete="new-password">
+        </div>
+    </div>
 
-<link rel="stylesheet" href="style/form-layout.css" >
-<form enctype="multipart/form-data" action="" method="post" id="form">
-<h1>Form to register</h1>
-<p>Username  <br> <input type="text" name="login" /></p>
-<p>Password  <br> <input type="password" name="password" /></p>
-<p>Confirm password  <br> <input type="password" name="password_confirmation" /></p>
-<p><input type="submit" value="REGISTER"></p>
-
+    <div class="field">
+        <div class="control">
+            <button class="button is-link">Submit</button>
+        </div>
+    </div>
 </form>
