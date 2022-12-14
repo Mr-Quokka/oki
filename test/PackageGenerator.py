5455858cdbf70e45df8e9eaf9d7563e95b7dd4b0
Lou <lou.valade@etu.uca.fr>
2022-12-14 14:11:24 +0100

    Ajoute la génération automatique avec les dépendances et des fichiers de test

--- test/PackageGenerator.py
+++ test/PackageGenerator.py
@@ -2,34 +2,45 @@ import os
 import random
 
 def createRepertory(i):
-    os.mkdir(f'TestGeneration/package_{i}')
-    os.mkdir(f'TestGeneration/package_{i}/src/')
+    os.mkdir(f'testPackages/package_{i}')
+    os.mkdir(f'testPackages/package_{i}/src/')
+    os.mkdir(f'testPackages/package_{i}/test/')
 
 
-def createFiles(i):
+def createSourcesFiles(i):
     for j in range(3):
-        file = open(f"TestGeneration/package_{i}/src/file_{j}.c","a")
+        file = open(f"testPackages/package_{i}/src/file_{j}.c","a")
         file.write("Je suis un fichier C")
+
+def createTestsFiles(i):
+    file = open(f"testPackages/package_{i}/test/test.c","a")
+    file.write("Je suis un fichier C de test")
 
 def createTomlFile(i):
     name = 'package_'+ str(i)
     version = '0.0.1'
     description = 'Package to test resolution algorithm'
     license = 'MIT'
+    dependency = random.randint(0,4)
 
-    file = open(f"TestGeneration/package_{i}/oki.toml","a")
+    while dependency == i :
+        dependency = random.randint(0,4)
+    
+
+    file = open(f"testPackages/package_{i}/oki.toml","a")
     file.write(
-        "[pacakage]\n" + 
+        "[pacakage]\n" +
         "name = " + '"' + name + '"\n' + 
         "version = " + '"' + version + '"\n' + 
         "description = " + '"' + description + '"\n' + 
-        "license = " + '"' + license + '"\n' +
+        "license = " + '"' + license + '"\n\n' +
 
         "[dependencies]\n" +
-        "package_" + random.randint(0,4) + " = " + version
+        "package_" + str(dependency) + " = " + '"' + version + '"'
         )
 
-for i in range(5):
+for i in range(1,6):
     createRepertory(i)
-    createFiles(i)
+    createSourcesFiles(i)
+    createTestsFiles(i)
     createTomlFile(i)
