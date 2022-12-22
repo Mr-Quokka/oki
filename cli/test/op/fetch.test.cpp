44438184067db089d341d5aa01188981cf35cba6
clfreville2 <clement.freville2@etu.uca.fr>
2022-12-22 11:23:09 +0100

    cli(test): Teste de télécharger un paquet

--- cli/test/op/fetch.test.cpp
+++ cli/test/op/fetch.test.cpp
@@ -1,11 +1,15 @@
 #include <doctest/doctest.h>
+#include <sstream>
 
+#include "io/Archive.h"
 #include "io/TmpFile.h"
 #include "op/fetch.h"
 #include "package/VersionLock.h"
 #include <fstream>
 #include <iostream>
 
+using namespace io;
+using namespace semver;
 using namespace op;
 
 TEST_CASE("fetch empty") {
@@ -13,4 +17,21 @@ TEST_CASE("fetch empty") {
     std::ofstream nullStream;
     nullStream.setstate(std::ios_base::badbit);
     fetch(std::unordered_map<std::string, package::VersionLock>{}, nullStream, {}, tmp.getDirname());
+}
+
+TEST_CASE("fetch one dependency") {
+    io::TmpDir tmpDir;
+    io::TmpFile dependencyFile;
+    Compressor compressor{dependencyFile.getFilename()};
+    compressor.compress(tmpDir.getDirname());
+
+    std::string url = "file:///";
+    url += dependencyFile.getFilename();
+
+    std::stringstream out;
+    CHECK_EQ(fetch(std::unordered_map<std::string, package::VersionLock>{
+                       {"foo", package::VersionLock{Version{5, 2, 3}, url}}},
+                   out, {}, tmpDir.getDirname()),
+             0);
+    CHECK_EQ(out.str(), "Installed 1 package\n");
 }
