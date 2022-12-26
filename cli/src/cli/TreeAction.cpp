ab3ddd8c68761bf71a3c8383c3a0e99e0d4fa97e
clfreville2 <clement.freville2@etu.uca.fr>
2022-12-26 12:20:01 +0100

    cli: Refactor avec #listDeclaredPackagesNames

--- cli/src/cli/TreeAction.cpp
+++ cli/src/cli/TreeAction.cpp
@@ -9,15 +9,7 @@ namespace cli {
         config::Manifest manifest = config::Manifest::fromFile(OKI_MANIFEST_FILE);
         config::ManifestLock manifestLock = config::ManifestLock::readOrResolve(OKI_MANIFEST_FILE, OKI_LOCK_FILE, repository);
 
-        std::vector<std::string> directDependencies;
-        std::transform(
-            manifest.listDeclaredPackages().cbegin(),
-            manifest.listDeclaredPackages().cend(),
-            std::back_inserter(directDependencies),
-            [](const auto &dep) {
-                return dep.first;
-            });
-
+        std::vector<std::string> directDependencies = manifest.listDeclaredPackagesNames();
         tree::TreeRenderer renderer{manifestLock, directDependencies};
         std::cout << renderer;
     }
