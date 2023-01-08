5ec72154c5c66299d98f32b8b179549117953eda
clfreville2 <clement.freville2@etu.uca.fr>
2023-01-08 20:48:33 +0100

    cli: Lis les identifiants dans des variables d'environnement
    Cette fonction vise à faciliter l'écriture de scripts automatisant
    oki publish.

--- cli/src/config/AuthProvider.cpp
+++ cli/src/config/AuthProvider.cpp
@@ -1,14 +1,33 @@
 #include "AuthProvider.h"
 
 #include <iostream>
+#include <termios.h>
 #include <unistd.h>
 
 namespace config {
     std::pair<std::string, std::string> AuthProvider::get() {
+        const char *username = std::getenv("OKI_USERNAME");
+        const char *password = std::getenv("OKI_PASSWORD");
+        if (username != nullptr && password != nullptr) {
+            return std::make_pair(username, password);
+        }
+        return read();
+    }
+
+    std::pair<std::string, std::string> AuthProvider::read() {
         std::cout << "Username: ";
         std::string username;
         std::getline(std::cin, username);
-        std::string password{getpass("Password: ")};
+
+        std::cout << "Password: ";
+        std::string password;
+        struct termios tty;
+        tcgetattr(STDIN_FILENO, &tty);
+        tty.c_lflag &= ~ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+        std::cin >> password;
+        tty.c_lflag |= ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &tty);
         return std::make_pair(std::move(username), std::move(password));
     }
 }
--- cli/src/config/AuthProvider.h
+++ cli/src/config/AuthProvider.h
@@ -4,6 +4,9 @@
 
 namespace config {
     class AuthProvider {
+    private:
+        std::pair<std::string, std::string> read();
+
     public:
         std::pair<std::string, std::string> get();
     };
