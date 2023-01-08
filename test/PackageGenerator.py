27e71ac4dd819831ed7c2e57fce4b978cca0df14
clfreville2 <clement.freville2@etu.uca.fr>
2023-01-08 19:57:26 +0100

    test: Refactor le générateur de paquets

--- test/PackageGenerator.py
+++ test/PackageGenerator.py
@@ -1,46 +1,66 @@
 import os
-import random
+import subprocess
+from random import randrange
 
-def createRepertory(i):
-    os.mkdir(f'testPackages/package_{i}')
-    os.mkdir(f'testPackages/package_{i}/src/')
-    os.mkdir(f'testPackages/package_{i}/test/')
+os.putenv('OKI_REPOSITORY', 'http://localhost:8000')
+os.putenv('OKI_USERNAME', 'test')
+os.putenv('OKI_PASSWORD', 'test')
+WORKING_DIRECTORY = '/tmp/test_packages'
 
 
-def createSourcesFiles(i):
-    for j in range(3):
-        with open(f"testPackages/package_{i}/src/file_{j}.c","a") as file:
-            file.write("Je suis un fichier C")
+def create_directory(package_name: str):
+    os.mkdir(f'{WORKING_DIRECTORY}/{package_name}')
+    os.mkdir(f'{WORKING_DIRECTORY}/{package_name}/src/')
+    os.mkdir(f'{WORKING_DIRECTORY}/{package_name}/test/')
 
-def createTestsFiles(i):
-    with open(f"testPackages/package_{i}/test/test.c","a") as file:
-        file.write("Je suis un fichier C de test")
 
-def createTomlFile(i):
-    name = 'package_'+ str(i)
-    version = '0.0.1'
-    description = 'Package to test resolution algorithm'
-    license = 'MIT'
-    dependency = random.randint(0,4)
+def create_sources_files(package_name: str, package_id: int):
+    with open(f'{WORKING_DIRECTORY}/{package_name}/src/{package_id}.c', 'w') as cfile:
+        cfile.write(f'int get{package_name}(void)')
+        cfile.write('{')
+        cfile.write(f'   return {package_id};')
+        cfile.write('}')
 
-    while dependency == i :
-        dependency = random.randint(0,4)
-    
+    with open(f'{WORKING_DIRECTORY}/{package_name}/src/{package_id}.h', 'w') as hfile:
+        hfile.write(f'int get{package_id}(void);')
 
-    with open(f"testPackages/package_{i}/oki.toml","a") as file:
-        file.write(
-            "[pacakage]\n" +
-            "name = " + '"' + name + '"\n' + 
-            "version = " + '"' + version + '"\n' + 
-            "description = " + '"' + description + '"\n' +
-            "license = " + '"' + license + '"\n\n' +
 
-            "[dependencies]\n" +
-            "package_" + str(dependency) + " = " + '"' + version + '"'
-            )
+def create_manifest(name: str, package_id: int, version: str):
+    description = 'Package to test the resolution algorithm'
 
-for i in range(1,6):
-    createRepertory(i)
-    createSourcesFiles(i)
-    createTestsFiles(i)
-    createTomlFile(i)
+    with open(f'{WORKING_DIRECTORY}/{name}/oki.toml', 'w') as file:
+        file.write(f"""[package]
+name = "{name}"
+version = "{version}"
+kind = "c"
+description = "{description}"
+license = "MIT"
+        
+[dependencies]
+""")
+
+        if package_id < 5:
+            return
+
+        for _ in range(randrange(package_id) // 5):
+            package = f'package_{randrange(package_id)}'
+            file.write(f'{package} = "{version}"\n')
+
+
+def publish_random(package_id: int, version: str = '0.1.0'):
+    name = f'package_{package_id}'
+    create_directory(name)
+    create_sources_files(name, package_id)
+    create_manifest(name, package_id, version)
+    os.chdir(f'{WORKING_DIRECTORY}/{name}')
+    subprocess.run(['oki', 'publish'])
+
+
+def main():
+    os.mkdir(WORKING_DIRECTORY)
+    for i in range(10):
+        publish_random(i)
+
+
+if __name__ == '__main__':
+    main()
