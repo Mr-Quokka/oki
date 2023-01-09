9897d6aeb66b267d73a58d7e7a4ba7163ed38834
clfreville2 <clement.freville2@etu.uca.fr>
2023-01-09 12:19:24 +0100

    web: Ajoute une balise meta viewport

--- web/views/layout.php
+++ web/views/layout.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/style/layout.css">
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <script src="/scripts/toggle-theme.js" defer></script>
