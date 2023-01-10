06461a86d715dd9838bb8333ab5653bff3d35889
Colin FRIZOT <Colin.FRIZOT@etu.uca.fr>
2023-01-10 09:22:11 +0100

    Met a jour le script avec oki publish

--- fill-repository.sh
+++ fill-repository.sh
@@ -2,6 +2,9 @@
 
 set -eu
 
+user="x"
+pswd="x"
+
 if [ -d packages ]; then
     cd packages && git pull --rebase --autostash && cd ..
 else
@@ -9,17 +12,9 @@ else
 fi
 
 dest=$(dirname $(realpath "$0"))
-requests=''
 for package in packages/*; do
     packageName=$(basename $package)
-    existingPackage="$dest"/web/public/packages/${packageName}_0.1.0.zip
-    rm -f "$existingPackage"
-    cd $package/src && zip -r "$existingPackage" ./*
+    echo "$package"
+    cd $package && echo $user $pswd | oki publish 
     cd -
-    size=$(stat --printf="%s" "$existingPackage")
-    requests="$requests
-    INSERT INTO package (name, description, language_id) VALUES ('$packageName', 'A random package', (SELECT id_language FROM language WHERE designation = 'c'));
-    INSERT INTO version (package_id, identifier, published_date, file_size) VALUES ((SELECT id_package FROM package WHERE name='$packageName'), '0.1.0', CURRENT_TIMESTAMP, $size);"
 done
-
-echo "$requests" | sqlite3 "$dest"/web/oki_packages.db
