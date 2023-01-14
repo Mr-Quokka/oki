5c1d4b913a19ff76de598b2cb36e66368e84a484
clfreville2 <clement.freville2@etu.uca.fr>
2023-01-14 12:21:29 +0100

    web: Corrige la pollution de la variable $version

--- web/views/package/versions.php
+++ web/views/package/versions.php
@@ -1,12 +1,12 @@
 <?php if (empty($params['package']->getVersions())): ?>
     <p class="error">This package doesn't have any version</p>
 <?php else: ?>
-    <?php foreach ($params['package']->getVersions() as $version): ?>
+    <?php foreach ($params['package']->getVersions() as $ver): ?>
         <div class="package">
             <h3>
-                <a class="package-name" href="<?= $version->getDownloadUrl() ?>"><?= $version->getIdentifier() ?></a>
+                <a class="package-name" href="<?= $ver->getDownloadUrl() ?>"><?= $ver->getIdentifier() ?></a>
             </h3>
-            <p class="package-description"> Published the <?= $version->getPublishedDate() ?></p>
+            <p class="package-description"> Published the <?= $ver->getPublishedDate() ?></p>
         </div>
     <?php endforeach ?>
 <?php endif ?>
