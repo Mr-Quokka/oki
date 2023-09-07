d0c19874efab644e52d2ac40d03b8c247927ae6f
clfreville2 <clement.freville2@etu.uca.fr>
2023-09-07 09:58:06 +0200

    cli: Mets à jour l'adresse du proxy

--- cli/make-in-vdn.sh
+++ cli/make-in-vdn.sh
@@ -22,13 +22,14 @@ fi
 currentDate=$(date -u +'%F %H:%M:%S')
 vdn-ssh root@$VDN_SYSTEM "date -s '$currentDate' &> /dev/null"
 
-rsync -av src test -e vdn-ssh root@$VDN_SYSTEM:
+rsync -av src test man contrib -e vdn-ssh root@$VDN_SYSTEM:
 vdn-scp configure.sh root@$VDN_SYSTEM:
 vdn-scp Makefile root@$VDN_SYSTEM:
 vdn-ssh root@$VDN_SYSTEM << EOF
 route del default gw 192.168.2.1
 route add default gw 10.0.2.2
-export http_proxy=http://193.49.118.36:8080/
+export http_proxy=http://proxycl.iut.uca.fr:8080
+export https_proxy=$http_proxy
 if ! pkg-config nlohmann_json --exists || ! pkg-config libcurl --exists || ! pkg-config minizip --exists || ! command -v gcovr &> /dev/null || ! [ -f /usr/include/doctest/doctest.h ] || ! [ -d /usr/share/doc/libboost-program-options-dev ]; then
   apt-get install -y nlohmann-json3-dev libcurl4-openssl-dev libminizip-dev libboost-program-options-dev doctest-dev gcovr
 fi
